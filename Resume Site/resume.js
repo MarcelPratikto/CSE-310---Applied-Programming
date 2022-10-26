f5187bca7d7b6c3d893cdad88c54b411773b4b7e
Marcel Pratikto <marcelaapratikto@gmail.com>
2022-10-26 00:14:51 -0600

    added createtable and addcolumn

--- Resume Site/resume.js
+++ Resume Site/resume.js
@@ -4,14 +4,59 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "!1RootPassword"
+    password: "!1RootPassword",
+    database: "mydb"
 });
 
-con.connect(function(err){
-    if (err) throw err;
-    console.log("Connected!");
+// initial database setup, probably won't need to use again
+// con.connect(function(err){
+//     if (err) throw err;
+//     console.log("Connected!");
+//     con.query("CREATE DATABASE mydb", function (err, result) {
+//         if (err) throw err;
+//         console.log("Database Created");
+//       });
+// });
+
+// creates a table in database
+// con: connection
+// name: name of table
+// col: array of columns
+// TODO if the loop doesn't work: name, type, b_date, e_date, desc
+function createTable(con, table_name, col){
+    col_string = ``
+    for(var i = 0; i < col.length; i++){
+        if (i == 0){
+            col_string += `(id INT AUTO_INCREMENT PRIMARY KEY, ${col[i]} VARCHAR(255), `;
+        } else if (i == col.length - 1) {
+            col_string += `${col[i]} VARCHAR(255))`;
+        } else {
+            col_string += `${col[i]} VARCHAR(255), `;
+        }
+    }
+    var sql = `CREATE TABLE ${table_name} ${col_string}`;
     con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("Result: " + result);
-      });
+        console.log("Table created");
+    });
+}
+
+// add column to a table
+function addColumn(con, table_name, col){
+    var sql = `ALTER TABLE ${table_name} ADD COLUMN ${col} VARCHAR(255)`;
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(`Added ${col} to ${table_name}`);
+    });
+}
+
+// start connect
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
 });
+var experience_col = ["company_name", "title", "location", "start_date", "end_date"]
+createTable(con, "experience", experience_col)
+addColumn(con, "experience", "description")
+// end connection
+con.end();
