5f9fc8dd2547102d754afd0358d43fa50c782795
Marcel Pratikto <marcelaapratikto@gmail.com>
2022-11-09 12:21:56 -0700

    Written Car, Factory, working on Dealer

--- Rust/hello.rs
+++ Rust/hello.rs
@@ -1,3 +1,95 @@
+//import the necessary modules
+use std::thread;
+use std::time::Duration;
+use cute::c;
+use rand::Rng;
+
+// Constants
+const MAX_QUEUE_SIZE:u8 = 10;
+const SLEEP_REDUCE_FACTOR:u8 = 50;
+const FACTORY_FINISHED_MESSAGE:&str = "!done!";
+
+// Car stuff
+struct Car{
+    model:String,
+    make:String,
+    year:u32
+}
+impl Car{
+    let car_makes = [
+        'Ford', 'Chevrolet', 'Dodge', 'Fiat', 'Volvo', 'Infiniti', 'Jeep', 'Subaru', 
+        'Buick', 'Volkswagen', 'Chrysler', 'Smart', 'Nissan', 'Toyota', 'Lexus', 
+        'Mitsubishi', 'Mazda', 'Hyundai', 'Kia', 'Acura', 'Honda'
+    ];
+    let car_models = [
+        'A1', 'M1', 'XOX', 'XL', 'XLS', 'XLE' ,'Super' ,'Tall' ,'Flat', 'Middle', 'Round',
+        'A2', 'M1X', 'SE', 'SXE', 'MM', 'Charger', 'Grand', 'Viper', 'F150', 'Town', 'Ranger',
+        'G35', 'Titan', 'M5', 'GX', 'Sport', 'RX'
+    ];
+    let car_years = c![i, for i in range 1990..2022];
+    
+    fn init(&self){
+        let mut rng = rand::thread_rng();
+        self.make = car_makes[rng.gen_range(0..car_makes.len())].to_string();
+        self.model = car_models[rng.gen_range(0..car_models.len())].to_string();
+        self.year = car_years[rng.gen_range(0..car_years.len())];
+    }
+
+    fn display(&self){
+        println!("{} {}, {}", self.make, self.model, self.year);
+    }
+}
+
+// Queue stuff
+struct Queue251{
+    items:Vec<Car>,
+    max_size:u32
+}
+impl Queue251{
+    fn init(&self){
+        self.items = vec![];
+        self.max_size = 0;
+    }
+    fn get_max_size(&self){
+        return self.max_size;
+    }
+    fn put(&self, item:Car){
+        self.items.push(item);
+        if self.items.len() > self.max_size{
+            self.max_size = self.items.len();
+        }
+    }
+    fn get(&self){
+        return self.items.pop();
+    }
+}
+
+struct Factory{
+    cars_to_produce:u32,
+    sem_available_slots:u32,
+    car_queue:Vec<Queue251>,
+    num_dealers:u32
+}
+impl Factory{
+    fn init(&self, car_queue:Vec<Queue251>, sem_available_slots:u32, num_dealers:u32){
+        let mut rng = rand::thread_rng();
+        self.cars_to_produce = rng.gen_range(200..300);
+        self.sem_available_slots = sem_available_slots;
+        self.num_dealers = num_dealers;
+    }
+    fn run(&self){
+        println!("PRODUCING {} CARS", self.cars_to_produce);
+        for i in self.cars_to_produce{
+            self.car_queue.put(Car.init());
+        }
+        println!("FINISHED MAKING CARS!!!");
+        for i in 0..num_dealers{
+            
+        }
+    }
+}
+
+// Run production
 fn run_production(factory_count:i32, dealer_count:i32) -> (f32,u32,Vec<u32>,Vec<u32>){
     
     let mut dealer_stats = vec![0;dealer_count];
